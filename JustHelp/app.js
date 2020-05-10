5a1038ce8d111097ff8b0ca435bea12d15c51ffe
elliott fisher <fisher0400@mail.philau.edu>
2020-05-10 05:53:14 -0400

    Add files via upload
    with only the first questionnaire done

--- JustHelp/app.js
+++ JustHelp/app.js
@@ -3,6 +3,24 @@ const ul_2 = document.querySelector(".option2");
 const ul_3 = document.querySelector(".option3");
 const ul_4 = document.querySelector(".option4");
 const ul_5 = document.querySelector(".option5");
+const ul_6 = document.querySelector(".option6");
+const ul_7 = document.querySelector(".option7");
+const ul_8 = document.querySelector(".option8");
+const ul_9 = document.querySelector(".option9");
+const ul_10 = document.querySelector(".option10");
+const ul_11 = document.querySelector(".option11");
+const ul_12 = document.querySelector(".option12");
+const ul_13 = document.querySelector(".option13");
+const ul_14 = document.querySelector(".option14");
+const ul_15 = document.querySelector(".option15");
+const ul_16 = document.querySelector(".option16");
+const ul_17 = document.querySelector(".option17");
+const ul_18 = document.querySelector(".option18");
+const ul_19 = document.querySelector(".option19");
+const ul_20 = document.querySelector(".option20");
+
+
+
 
 
 
@@ -11,6 +29,21 @@ const q2 = document.querySelector(".q2");
 const q3 = document.querySelector(".q3");
 const q4 = document.querySelector(".q4");
 const q5 = document.querySelector(".q5");
+const q6 = document.querySelector(".q6");
+const q7 = document.querySelector(".q7");
+const q8 = document.querySelector(".q8");
+const q9 = document.querySelector(".q9");
+const q10 = document.querySelector(".q10");
+const q11 = document.querySelector(".q11");
+const q12 = document.querySelector(".q12");
+const q13 = document.querySelector(".q13");
+const q14 = document.querySelector(".q14");
+const q15 = document.querySelector(".q15");
+const q16 = document.querySelector(".q16");
+const q17 = document.querySelector(".q17");
+const q18 = document.querySelector(".q18");
+const q19 = document.querySelector(".q19");
+const q20 = document.querySelector(".q20");
 
 
 const survey = document.querySelector(".survey");
@@ -54,12 +87,112 @@ ul_5.addEventListener("click", function() {
   q6 .style.display = "block";
 
 });
+//  question 6
+
+ul_6.addEventListener("click", function() {
+  q6.style.display = "none";
+  q7 .style.display = "block";
+
+});
+//  question 7
+
+ul_7.addEventListener("click", function() {
+  q7.style.display = "none";
+  q8 .style.display = "block";
+
+});
+//  question 8
+
+ul_8.addEventListener("click", function() {
+  q8.style.display = "none";
+  q9 .style.display = "block";
+
+});
+//  question 9
+
+ul_9.addEventListener("click", function() {
+  q9.style.display = "none";
+  q10 .style.display = "block";
+
+});
+//  question 10
+
+ul_10.addEventListener("click", function() {
+  q10.style.display = "none";
+  q11 .style.display = "block";
+
+});
+//  question 11
+
+ul_11.addEventListener("click", function() {
+  q11.style.display = "none";
+  q12 .style.display = "block";
+
+});
+//  question 12
+
+ul_12.addEventListener("click", function() {
+  q12.style.display = "none";
+  q13 .style.display = "block";
+
+});
+//  question 13
+
+ul_13.addEventListener("click", function() {
+  q13.style.display = "none";
+  q14 .style.display = "block";
+
+});
+//  question 14
+
+ul_14.addEventListener("click", function() {
+  q14.style.display = "none";
+  q15 .style.display = "block";
+
+});
+//  question 15
+
+ul_15.addEventListener("click", function() {
+  q15.style.display = "none";
+  q16 .style.display = "block";
+
+});
+//  question 16
+
+ul_16.addEventListener("click", function() {
+  q16.style.display = "none";
+  q17 .style.display = "block";
+
+});
+//  question 17
+
+ul_17.addEventListener("click", function() {
+  q17.style.display = "none";
+  q18 .style.display = "block";
+
+});
+//  question 18
+
+ul_18.addEventListener("click", function() {
+  q18.style.display = "none";
+  q19 .style.display = "block";
+
+});
+//  question 19
+
+ul_19.addEventListener("click", function() {
+  q19.style.display = "none";
+  q20 .style.display = "block";
+
+});
+//  question 20 (end)
+
+ul_20.addEventListener("click", function() {
+  q20.style.display = "none";
+  end.style.display = "block";
+  var timer = setTimeout(function() {
+            window.location='loading-POA-LovedOne.html'
+        }, 5000);
 
 
-// end questionnaire
-
-// ul_3.addEventListener("click", function() {
-//  q3.style.display = "none";
-//  survey.style.display = "none";
-//  end.style.display = "block";
-// });
+});
